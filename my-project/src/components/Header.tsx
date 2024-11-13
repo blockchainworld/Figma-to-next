c1f0b4acb62c9aad639c19d5898e9939ee667140
谢伟杰 <96247703+xwjisfather@users.noreply.github.com>
2024-11-13 18:45:53 +0800

    Update Header.tsx

--- my-project/src/components/Header.tsx
+++ my-project/src/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useAuth } from '../context/AuthContext';
 import Login from './Login';
 import Register from './Register';
